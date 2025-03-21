c73e9f63b23398678cb8853c4bc2a850a87f9388
hudanadeem <hudanadeem.02@gmail.com>
2025-03-21 10:46:50 -0400

    simplified footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
-import "./Footer.scss";
+import "./Footer.scss"; // Import the SCSS file
+import FaFacebook from "../../assets/Icons/facebook.png";
+import FaInstagram from "../../assets/Icons/instagram.png";
+import FaTwitter from "../../assets/Icons/twitter.png";
+import brandLogo from "../../assets/logos/skintuition_logo.png";
 
-function Footer(){
-    return(
-        <>
-        </>
-    )
-}
+const Footer = () => {
+  return (
+    <div className="footer__copyright">
+      © 2025 Skintuition. All rights reserved.
+    </div>
+  );
+};
 
 export default Footer;
