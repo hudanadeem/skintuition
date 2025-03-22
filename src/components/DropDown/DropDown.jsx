496b4881e913cc1b5a87e6f2325aa60de7ec5413
hudanadeem <hudanadeem.02@gmail.com>
2025-03-22 10:33:33 -0400

    func and styling for profile complete

---
+++ src/components/DropDown/DropDown.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import profileImage from "../../assets/Icons/logged-in.png";
+import "./DropDown.scss";
+
+function DropDown({ handleLogout }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [userData, setUserData] = useState({ name: "", email: "" });
+
+  const toggleDropdown = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        if (!token) return;
+
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/users/profile`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const { name, email } = response.data;
+        setUserData({ name, email });
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
+
+  return (
+    <div className="profile" onClick={toggleDropdown}>
+      <div className="profile__info">
+        <div className="user">
+          <h3>{userData.name || "Loading..."}</h3>
+          <p>{userData.email || " "}</p>
+        </div>
+        <div className="img-box">
+          <img src={profileImage} alt="user" />
+        </div>
+      </div>
+
+      <div className={`menu ${menuOpen ? "active" : ""}`}>
+        <ul>
+          <li>Profile</li>
+          <li>Inbox</li>
+          <li>Settings</li>
+          <li>Help</li>
+          <li>
+            <button onClick={handleLogout}>Sign Out</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default DropDown;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,16 +3,93 @@ import Footer from "../../components/Footer/Footer";
 import OurMission from "../../components/OurMission/OurMission";
 import NavBar from "../../components/NavBar/NavBar";
 import Hero from "../../components/Hero/Hero";
+import brandLogo from "../../assets/logos/skintuition_logo.png";
+import profileIcon from "../../assets/Icons/user-profile.png";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import "./HomePage.scss";
+import DropDown from "../../components/DropDown/DropDown";
 
 function HomePage() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [titleClass, setTitleClass] = useState("");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const viewportHeight = window.innerHeight;
+
+      if (scrollY > viewportHeight) {
+        setIsScrolled(true);
+        setTitleClass("in-nav");
+      } else {
+        setIsScrolled(false);
+        setTitleClass("");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <NavBar />
+      <div className="homepage">
+        <div className="homepage__container">
+          <div className={`homepage__nav ${isScrolled ? "scrolled" : ""}`}>
+            {/* Logo */}
+            <Link to="/">
+              <img
+                className="homepage__logo"
+                src={brandLogo}
+                alt="Brand Logo"
+              />
+            </Link>
+
+            {/* Profile Icon: conditional based on login status */}
+            <div className="homepage__profile">
+              {isLoggedIn ? (
+                <DropDown handleLogout={handleLogout}/>
+              ) : (
+                <Link to="/login">
+                  <button className="homepage__profile-button">
+                    <img
+                      className="homepage__profile-icon"
+                      src={profileIcon}
+                      alt="Default Profile Icon"
+                    />
+                  </button>
+                </Link>
+              )}
+            </div>
+          </div>
+
+          <h1 className={`homepage__title ${titleClass}`}>Skintuition</h1>
+        </div>
+      </div>
+
+      <OurMission />
+
+      {/* <NavBar />
       <Hero />
       <OurMission />
       <FeatureCards />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
